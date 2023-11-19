b974ad871ea72e8fbdb3301d68f498dce71c2196
Ghostintheshell <111326458+Ghostintheshell-iuseArchbtw@users.noreply.github.com>
2023-11-19 00:21:09 -0500

    git copilot

--- PortPwnedbinary-hex-ASCII-text.py
+++ PortPwnedbinary-hex-ASCII-text.py
@@ -1,8 +1,5 @@
 import os
-import binascii
 import chardet
-import argparse
-import textwrap
 
 def analyze_binary_data(file_path, analysis_mode='all'):
     try:
@@ -14,7 +11,7 @@ def analyze_binary_data(file_path, analysis_mode='all'):
         with open(file_path, 'rb') as binary_file:
             data = binary_file.read()
 
-            if analysis_mode == 'info' or analysis_mode == 'all':
+            if analysis_mode == 'info':
                 print(f"File Information:")
                 display_file_info(file_path, data)
 
@@ -22,25 +19,25 @@ def analyze_binary_data(file_path, analysis_mode='all'):
                 print(f"Byte Statistics:")
                 display_byte_statistics(data)
 
-            hex_representation = binascii.hexlify(data).decode('utf-8').upper()
+            hex_representation = data.hex().upper()  # Convert binary data to hexadecimal
             print("\nHexadecimal representation of binary data:")
             print(hex_representation)
 
-            if analysis_mode == 'repeating' or analysis_mode == 'all':
+            if analysis_mode == 'all' or analysis_mode == 'repeating':
+                # Attempt to find repeating patterns (4-byte sequences) in the hexadecimal data
                 find_repeating_patterns(hex_representation)
 
-            if analysis_mode == 'ascii' or analysis_mode == 'all':
-                try:
-                    ascii_data = data.decode('ascii', errors='replace')
-                    print("\nASCII representation of the data:")
-                    print(interpret_ascii_data(ascii_data))
-                except UnicodeDecodeError:
-                    print("\nUnable to decode binary data as ASCII.")
+            if analysis_mode == 'all' or analysis_mode == 'ascii':
+                # Convert binary data to ASCII
+                ascii_data = data.decode('ascii', errors='replace')
+                print("\nASCII representation of the data:")
+                print(interpret_ascii_data(ascii_data))
 
-            if analysis_mode == 'encoding' or analysis_mode == 'all':
+            if analysis_mode == 'all' or analysis_mode == 'encoding':
+                # Detect encoding using chardet
                 encoding_result = chardet.detect(data)
                 detected_encoding = encoding_result['encoding']
-                print(f"\nDetected encoding: {detected_encoding}")
+                print(f"Detected encoding: {detected_encoding}")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
@@ -53,7 +50,7 @@ def display_file_info(file_path, data):
 def display_byte_statistics(data):
     byte_counts = {}
     for byte in data:
-        byte_hex = format(byte, '02X')
+        byte_hex = format(byte, '02X')  # Convert byte to uppercase hexadecimal
         if byte_hex in byte_counts:
             byte_counts[byte_hex] += 1
         else:
@@ -65,8 +62,8 @@ def display_byte_statistics(data):
 
 def find_repeating_patterns(hex_data):
     print("\nRepeating 4-byte patterns:")
-    patterns = {}
-    pattern_length = 8
+    patterns = {}  # Dictionary to store patterns and their counts
+    pattern_length = 8  # 8 characters represent 4 bytes in hexadecimal
 
     for i in range(len(hex_data) - pattern_length + 1):
         pattern = hex_data[i:i + pattern_length]
@@ -75,28 +72,18 @@ def find_repeating_patterns(hex_data):
         else:
             patterns[pattern] = 1
 
+    # Print patterns that repeat more than once
     for pattern, count in patterns.items():
         if count > 1:
             print(f"Pattern: {pattern} | Count: {count}")
 
 def interpret_ascii_data(ascii_data):
+    # Replace control characters and non-printable characters with spaces
     printable_data = ''.join(char if 32 <= ord(char) <= 126 else ' ' for char in ascii_data)
     return printable_data
 
-def main():
-    parser = argparse.ArgumentParser(
-        description='Analyze binary data in a file.',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent('''
-        Example:
-        analyze_binary_data /path/to/binary/file --analysis-mode all
-        ''')
-    )
-    parser.add_argument('file_path', help='Path to the binary file to analyze')
-    parser.add_argument('--analysis-mode', choices=['all', 'info', 'stats', 'repeating', 'ascii', 'encoding'],
-                        default='all', help='Choose analysis mode (default: all)')
-    args = parser.parse_args()
-    analyze_binary_data(args.file_path, args.analysis_mode)
-
 if __name__ == "__main__":
-    main()
+    file_path = input("Enter the path to your binary file: ")  # Prompt the user for the binary file path
+    analysis_mode = input("Choose analysis mode ('all', 'info', 'stats', 'repeating', 'ascii', 'encoding'): ").lower()
+    analyze_binary_data(file_path, analysis_mode)
+
